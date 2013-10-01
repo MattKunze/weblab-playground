c361d2ac3cb4775d7c79c1518308329c383c9e8a
Matt Kunze <matt.kunze@datasplice.com>
2013-09-30 18:58:12 -0600

    adding interface for filterWordLength function

--- functions.js
+++ functions.js
@@ -13,3 +13,14 @@ var reverseString = function(text) {
 }
 document.write('<p>hello -> ' + reverseString('hello') + '</p>');
 document.write('<p>a longer example -> ' + reverseString('a longer example') + '</p>');
+
+document.write('<h4>Filter Text Array by Length');
+
+/*
+ * Takes an input array of text items and returns a list of the
+ * elements that are longer than length
+ */
+var filterWordLength = function(list, length) {
+  var result = [];
+  return result;
+}
