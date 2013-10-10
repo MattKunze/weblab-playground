09ee1d229bd7050829916a06a7355f3f01a88aca
Matt Kunze <matt.kunze@datasplice.com>
2013-10-09 20:57:46 -0600

    initial todo files
    adding items, marking complete, and tracking remaining count is working

---
+++ todo.js
@@ -0,0 +1,39 @@
+var templates = {
+  todoItem:
+  		'<div class="todo-item wide">' +
+        '  <span class="toggle">&gt;</span>' +
+        '  <label></label>' +
+        '</div>'
+};
+
+var app = {
+  init: function() {
+    $('.new-todo').keypress(app.addNewItem);
+    $('.todo-list').on('click', '.toggle', app.toggleStatus);
+  },
+  addNewItem: function(ev) {
+    // when keypress is enter
+    if(ev.which === 13) {
+      // get the new todo label
+      var label = $('.new-todo').val();
+      // create new todo item and append
+      var newItem = $(templates.todoItem)
+      // update the label
+      newItem.find('label').text(label)
+      // append to list
+      newItem.appendTo('.todo-list');
+      // reset todo input
+      $('.new-todo').val('');
+      // update remaining
+      app.updateRemaining();
+    }
+  }, 
+  toggleStatus: function(ev) {
+    $(ev.target).closest('.todo-item').toggleClass('complete');
+    app.updateRemaining();
+  },
+  updateRemaining: function() {
+    var remaining = $('.todo-item').not('.complete');
+    $('.remaining').text(remaining.length + ' remaining');
+  }
+}
